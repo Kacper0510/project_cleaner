c03d314771c7d47fa7fd40033c5b488d6448ce0a
jaanonim <mat8mro@gmail.com>
2024-08-23 15:43:28 +0200

    better info

--- src/ui/render/popup/info.rs
+++ src/ui/render/popup/info.rs
@@ -1,12 +1,12 @@
 use ratatui::{
-    layout::Rect,
-    style::{Color, Style, Stylize},
+    layout::{self, Constraint, Layout, Rect},
+    style::{Color, Modifier, Style, Stylize},
     text::{Line, Span},
-    widgets::{Block, BorderType, Padding, Paragraph},
+    widgets::{Block, BorderType, Padding, Paragraph, Row, Table},
     Frame,
 };
 
-use crate::ui::app::App;
+use crate::ui::{app::App, model::MatchGroup};
 
 use super::make_popup_layout;
 
@@ -15,6 +15,11 @@ pub fn render(app: &mut App, frame: &mut Frame, area: Rect) -> Option<()> {
     let match_data = app.table.get_by_path(&path)?;
 
     let area = make_popup_layout(frame, area);
+    let layout = Layout::default()
+        .direction(layout::Direction::Vertical)
+        .constraints(vec![Constraint::Fill(1), Constraint::Fill(1)])
+        .margin(2)
+        .split(area);
     let small_style = Style::default().fg(Color::DarkGray);
 
     let mut text = vec![
@@ -38,28 +43,36 @@ pub fn render(app: &mut App, frame: &mut Frame, area: Rect) -> Option<()> {
         })
         .collect();
     text.append(&mut other);
-    text.append(&mut vec![
-        Line::from(vec![]),
-        Line::from(vec![Span::styled("Stats: ", Style::default().bold())]),
-        Line::from(vec![
-            Span::from("Size: "),
-            Span::styled(match_data.stats().size.map(|s| format!("{}", s)).unwrap_or("---".to_owned()), small_style),
-        ]),
-        Line::from(vec![
-            Span::from("Last modification: "),
-            Span::styled(
-                match_data.stats().last_mod_days().map(|s| format!("{}d", s)).unwrap_or("---".to_owned()),
-                small_style,
-            ),
-        ]),
-    ]);
 
-    let container = Paragraph::new(text).block(
+    frame.render_widget(
         Block::bordered()
             .border_type(BorderType::Rounded)
             .border_style(Style::default().fg(Color::Cyan))
             .padding(Padding::uniform(1)),
+        area,
     );
-    frame.render_widget(container, area);
+
+    let container = Paragraph::new(text);
+    frame.render_widget(container, layout[0]);
+
+    let widths = [Constraint::Percentage(100), Constraint::Length(10), Constraint::Length(10)];
+    let table = Table::new(match_data_to_rows(match_data), widths)
+        .column_spacing(1)
+        .header(Row::new(vec!["Path", "LastMod", "Size"]).style(Style::default().add_modifier(Modifier::BOLD)));
+    frame.render_widget(table, layout[1]);
+
     Some(())
+}
+
+fn match_data_to_rows(data: &MatchGroup) -> Vec<Row> {
+    data.matches
+        .iter()
+        .map(|ele| {
+            Row::new(vec![
+                ele.path.display().to_string(),
+                if let Some(s) = &ele.dir_stats.last_mod_days() { format!("{}d", s) } else { "---".to_owned() },
+                if let Some(s) = &ele.dir_stats.size { format!("{}", s) } else { "---".to_owned() },
+            ])
+        })
+        .collect::<Vec<_>>()
 }
